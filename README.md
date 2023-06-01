eeaee9a13358114942b28354f49e27350d28af2a
HEESUNae <heesun9456@gmail.com>
2023-06-01 13:54:08 +0900

    add OpenGraph

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-## Next.js 13 버전 CRUD 기능 구현
+## Next.js 13, 무료 API를 활용한 리스트 구현
 
+- 사용언어 : HTML, CSS, TypeScript
 - 프레임워크 : Next.js 13
 - 상태관리 : Jotai
 - 라이브러리 : Styled-components, SCSS, Axios
+- 배포 : vercel
 
 ### 구현한 기능 설명
 
@@ -11,5 +13,6 @@
 - 무료 API 호출하여 리스트 페이지에 데이터 맵핑
 - 데이터 10개씩 나눠서 페이징 기능 구현
 - 상세보기 페이지로 이동시 해당 내용 렌더링
+- useRouter를 활용해 이전페이지로 이동
 
 [jsonplaceholder]: https://jsonplaceholder.typicode.com/
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,23 @@ import styles from "@/styles/page.module.scss";
 
 export const metadata: Metadata = {
   title: "Rabbit Posts",
-  description: "무료 API를 활용하여 포트폴리오 용도로 만든 개인작업물입니다.",
+  description: "무료 API를 활용하여 포트폴리오 용도로 만든 개인 작업물입니다.",
+  authors: [{ name: "Heesun" }, { name: "Hwang", url: "https://nextjs-rabbit.vercel.app/" }],
+  openGraph: {
+    title: "서포터 토깽이의 블로그",
+    description: "무료 API를 활용하여 포트폴리오 용도로 만든 개인 작업물입니다.",
+    url: "https://nextjs-rabbit.vercel.app",
+    siteName: "서포터 토깽이의 블로그",
+    images: [
+      {
+        url: "/assets/image/bg-main.jpg",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
 };
 
 export default function Home() {
